fb26f96c62e51b569f74cc4d032b4cc7556ecf03
Rishab Nair <rishabnair2001@gmail.com>
2019-02-16 13:10:23 +0000

    update to lineagehw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
 BOARD_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DCAPRI_HWC -DREFBASE_JB_MR1_COMPAT_SYMBOLS
 
 # LINEAGEHW
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/i9152/cmhw/
+BOARD_HARDWARE_CLASS := hardware/samsung/lineagehw/ device/samsung/i9152/cmhw/
 
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/i9152/ril/
