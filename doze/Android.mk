1e7e7543dcad9d817874a51be569d539fceb5f0f
Rishab Nair <rishabnair2001@gmail.com>
2019-02-23 16:24:44 +0530

    set local sdk version

--- doze/Android.mk
+++ doze/Android.mk
@@ -4,8 +4,10 @@ LOCAL_MODULE_TAGS := optional
  
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
  
-LOCAL_PACKAGE_NAME := SamsungDoze LOCAL_CERTIFICATE := platform 
-LOCAL_PRIVATE_PLATFORM_APIS := true LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PACKAGE_NAME := SamsungDoze 
+LOCAL_CERTIFICATE := platform 
+LOCAL_PRIVATE_PLATFORM_APIS := true
+ LOCAL_PRIVILEGED_MODULE := true
  
 LOCAL_USE_AAPT2 := true
  
